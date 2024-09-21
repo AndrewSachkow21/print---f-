2951a1103d5cf0d6a719509dfa7fda162826f348
AndrewSachkow21 <bravlerkrutenko@gmail.com>
2024-09-21 18:24:36 +0300

    update

--- task2-4.py
+++ task2-4.py
@@ -2,10 +2,10 @@ import random
 
 def generate_ticket(last_name, destination, departure_time):
   ticket_number = random.randint(100000, 999999)
-  return f"Квиток №{ticket_number}\nПрізвище: {last_name}\nПункт призначення: {destination}\nЧас відправлення: {departure_time}\nmicце: {departure_time2}"
+  return f"Квиток №{ticket_number}\nПрізвище: {last_name}\nПункт призначення: {destination}\nЧас відправлення: {departure_time}\nmicце: {transpor_place}"
 
 last_name = input("Введіть ваше прізвище: ")
 destination = input("Введіть пункт призначення: ")
 departure_time = input("Введіть час відправлення: ")
-departure_time2 = input("Введіть мicце у транспортi: ")
+transpor_place = input("Введіть мicце у транспортi: ")
 print(generate_ticket(last_name, destination, departure_time))
