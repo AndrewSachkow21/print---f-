0cee883e05848d6db5ed8041b3181aa9bd34cafe
AndrewSachkow21 <bravlerkrutenko@gmail.com>
2024-09-21 18:06:13 +0300

    update tusk

--- task2-3.py
+++ task2-3.py
@@ -1,4 +1,5 @@
 number = int(input("Мiнiмальне число для дiапозону таблиці множення: "))
-for b in range(int(input("максимальне число для дiапозону 1таблиці множення:"))-number + 1):
+v = int(input("максимальне число для дiапозону 1таблиці множення:"))-number + 1
+for b in range(v):
     for i in range(1, 11):
         print(f"{b + number} x {i} = {b + number * i}")
